8b93b2743996223da11a31c01bf9a8b380f28f97
Alexandre Martins Costa <alexandre.martinscosta@gmail.com>
2022-07-18 18:29:54 -0300

    operadores logicos

---
+++ operadores_logicos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (void)
+{
+    float nota_final, frequencia;
+
+    printf("Digite a nota final do aluno: ");
+    scanf("%f", &nota_final);
+
+    printf("Informe a frequência do aluno: ");
+    scanf("%f", &frequencia);
+
+    if (nota_final >= 6.0 && frequencia >= 75)
+        printf("Aluno aprovado \n");
+    else
+        printf("Aluno reprovado \n");
+
+    return (0);
+}
