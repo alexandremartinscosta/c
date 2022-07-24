8b2c7989266eb1adcc62f56bb579ff19379799d6
alexandremartinscosta <alexandre.martinscosta@gmail.com>
2022-07-24 16:32:51 -0300

    adicionando o programa chute_um_numero.c

---
+++ chute_um_numero.c
@@ -0,0 +1,24 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <time.h>
+
+int main(void)
+{
+	int i, aleatorio, num;
+	time_t t1;
+
+	srand((unsigned) time (&t1));
+	
+	for (i = 0; i < 1; i++)
+	{
+		aleatorio = ("%d", rand() % 10);
+		printf("Tente acertar o número aleatório de 1 a 10:\n");
+		scanf("%d", &num);
+		if (num == aleatorio)
+			printf("Você acertou!\n");
+		else
+			printf("Você errou...\n");
+	}
+
+	return 0;
+}
