d4751d1a89f7b9efe34d85bbd99852a5cdaf9701
alexandremartinscosta <alexandre.martinscosta@gmail.com>
2022-07-25 18:14:44 -0300

    correção: iteração desnecessária

--- chute_um_numero.c
+++ chute_um_numero.c
@@ -8,17 +8,15 @@ int main(void)
 	time_t t1;
 
 	srand((unsigned) time (&t1));
-	
-	for (i = 0; i < 1; i++)
-	{
-		aleatorio = ("%d", rand() % 10);
-		printf("Tente acertar o número aleatório de 1 a 10:\n");
-		scanf("%d", &num);
-		if (num == aleatorio)
-			printf("Você acertou!\n");
-		else
-			printf("Você errou...\n");
-	}
+	aleatorio = ("%d", rand() % 10);
+
+	printf("Tente acertar o número aleatório de 1 a 10:\n");
+	scanf("%d", &num);
+
+	if (num == aleatorio)
+		printf("Você acertou!\n");
+	else
+		printf("Você errou...\n");
 
 	return 0;
 }
