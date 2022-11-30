e2832a11bcc6cc7149c37082062332692d458063
alexandremartinscosta <alexandre.martinscosta@gmail.com>
2022-11-29 22:31:56 -0300

    função void

--- calculadora.c
+++ calculadora.c
@@ -4,26 +4,26 @@
 #include <string.h>
 #include <locale.h>
 
-int main() {
+void main() {
     
-    char primeiro_numero[1024], segundo_numero[1024], opcao_menu[1024], opcao_sair[1024], *endptr;
+    char primeiro_numero[1024], segundo_numero[1024], opcao_menu[1024], opcao_sair[1024], *ponteiro_final;
     int operacao, valido, continuar;
     float a, b;
 
     setlocale(LC_CTYPE, "Portuguese");
-    wprintf(L"\nBem-vindo à calculadora escrita em C!\n");
+    printf("\nBem-vindo à calculadora escrita em C!\n");
 
     inicio:
     do { 
-        wprintf(L"\nDigite o primeiro número: ");
+        printf("\nDigite o primeiro número: ");
         fgets(primeiro_numero, 1024, stdin);
-        a = strtof(primeiro_numero, &endptr);
+        a = strtof(primeiro_numero, &ponteiro_final);
 
-        if (primeiro_numero == endptr) {
-            wprintf(L"\nValor inválido. Por favor, digite um número inteiro ou decimal.\n");
+        if (primeiro_numero == ponteiro_final) {
+            printf("\nValor inválido. Por favor, digite um número inteiro ou decimal.\n");
             valido = 0;
         } else if (strchr(primeiro_numero, ',') != NULL) {
-            wprintf(L"\nPor favor, separe a casa decimal com o ponto.\n");
+            printf("\nPor favor, separe a casa decimal com o ponto.\n");
             valido = 0;
         } else {
             valido = 1;
@@ -31,12 +31,12 @@ int main() {
     } while (!valido);
         
     do {
-        wprintf(L"\nQue operação deseja realizar?\n\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Porcentagem\n6 - Potenciação\n\nOpção: ");
+        printf("\nQue operação deseja realizar?\n\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Porcentagem\n6 - Potenciação\n\nOpção: ");
         fgets(opcao_menu, 1024, stdin);
-        operacao = strtol(opcao_menu, &endptr,  10);
+        operacao = atoi(opcao_menu);
 
-        if (opcao_menu == endptr || operacao < 1 || operacao > 6) {
-            wprintf(L"\nOpção inválida. Por favor, digite um número do menu.\n");
+        if (strlen(opcao_menu) != 2 || operacao < 1 || operacao > 6) {
+            printf("\nOpção inválida. Por favor, digite um número do menu.\n");
             valido = 0;    
         } else {
             valido = 1;
@@ -45,15 +45,15 @@ int main() {
 
     meio:
     do {
-        wprintf(L"\nDigite o segundo número: ");
+        printf("\nDigite o segundo número: ");
         fgets(segundo_numero, 1024, stdin);
-        b = strtof(segundo_numero, &endptr);
+        b = strtof(segundo_numero, &ponteiro_final);
 
-        if (segundo_numero == endptr) {
-            wprintf(L"\nValor inválido. Por favor, digite um número inteiro ou decimal.\n");
+        if (segundo_numero == ponteiro_final) {
+            printf("\nValor inválido. Por favor, digite um número inteiro ou decimal.\n");
             valido = 0;
         } else if (strchr(segundo_numero, ',') != NULL) {
-            wprintf(L"\nPor favor, separe a casa decimal com o ponto.\n");
+            printf("\nPor favor, separe a casa decimal com o ponto.\n");
             valido = 0;
         } else {
             valido = 1;
@@ -62,39 +62,37 @@ int main() {
 
     switch(operacao) {
         case 1:
-            wprintf(L"\n%f mais %f é igual a: %f\n", a, b, a + b);
+            printf("\n%f mais %f é igual a: %f\n", a, b, a + b);
             break;
         case 2:
-            wprintf(L"\n%f menos %f é igual a: %f\n", a, b, a - b);
+            printf("\n%f menos %f é igual a: %f\n", a, b, a - b);
             break;
         case 3:
-            wprintf(L"\n%f vezes %f é igual a: %f\n", a, b, a * b);
+            printf("\n%f vezes %f é igual a: %f\n", a, b, a * b);
             break;
         case 4:
             if (b == 0) {
-                wprintf(L"\nNão é possível dividir por zero. Por favor, escolha um divisor válido.\n");
+                printf("\nNão é possível dividir por zero. Por favor, escolha um divisor válido.\n");
                 goto meio;
             } else {
-                wprintf(L"\n%f dividido por %f é igual a: %f\n", a, b, a / b);
+                printf("\n%f dividido por %f é igual a: %f\n", a, b, a / b);
                 break;
             }
         case 5:
-            wprintf(L"\nA porcentagem de %f em %f é igual a: %f%%\n", a, b, a / b * 100);
+            printf("\nA porcentagem de %f em %f é igual a: %f%%\n", a, b, a / b * 100);
             break;
         case 6:
-            wprintf(L"\n%f elevado à potência %f é igual a: %f\n", a, b, pow(a,b));
+            printf("\n%f elevado à potência %f é igual a: %f\n", a, b, pow(a,b));
             break;
-        default:
-            return 0;
     }
 
     do {
-        wprintf(L"\nDeseja realizar outra operação?\n\n1 - Sim\n2 - Não\n\nOpção: ");
+        printf("\nDeseja realizar outra operação?\n\n1 - Sim\n2 - Não\n\nOpção: ");
         fgets(opcao_sair, 1024, stdin);
-        continuar = strtol(opcao_sair, &endptr,  10);
+        continuar = atoi(opcao_sair);
 
-        if (opcao_sair == endptr || continuar < 1 || continuar > 2) {
-            wprintf(L"\nOpcao inválida. Por favor, digite um número do menu.\n");
+        if (strlen(opcao_sair) != 2 || continuar < 1 || continuar > 2) {
+            printf("\nOpcao inválida. Por favor, digite um número do menu.\n");
             valido = 0;    
         } else if (continuar == 1) {
             goto inicio;
